41b7286a4a8d32ee47de458db464ff6c371a5bea
Josue Guzman <josueguzman@Josues-MacBook-Pro-2.local>
2018-04-23 09:59:06 -0400

    added google oauth

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/key');
+
 const app = express();
   // This app object is set up to listen for incoming request
 
-app.get('/', (req, res) => {
-  res.send({ hi: "there" })
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+  (accessToken, refreshToken, profile, done) => {
+    console.log('accessToken', accessToken);
+    console.log('refreshToken', refreshToken);
+    console.log('profile', profile);
+   }
+  )
+);
 
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email', 'image']
+  })
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 

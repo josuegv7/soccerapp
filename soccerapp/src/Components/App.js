411042c7f0866f5a8f37f363d14d0d34ddc6897d
Josue Guzman <josueguzman@Josues-MacBook-Pro-2.local>
2018-04-25 12:55:22 -0400

    connect frontend to backend

--- soccerapp/src/Components/App.js
+++ soccerapp/src/Components/App.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import NavBar from './navbar';
+import Landing from './Landing';
 import Profile from './Profile';
-import Event from './Event';
+import Game from './Event';
 import CreateEvent from './EventCreate';
 
 
 
-class App extends Component {
+
+export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar/>
-        {/* <Profile/> */}
-        {/* <Event/> */}
-        <CreateEvent/>
+        <BrowserRouter>
+          <div>
+            <NavBar/>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/game" component={Game} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/game/new" component={CreateEvent} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
 }
-
-export default App;
--- soccerapp/src/index.js
+++ soccerapp/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
-import './index.css';
 import App from './Components/App';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={Store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));

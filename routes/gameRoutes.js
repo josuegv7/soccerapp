d630b8d5d798a0abd096d7777f0136c7b9182e35
Josue Guzman <josueguzman@Josues-MacBook-Pro-2.local>
2018-05-14 18:59:05 -0400

    webhooke fixed

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -1,5 +1,4 @@
 const _ = require('lodash');
-
 const Path = require('path-parser').default;
 const { URL } = require('url');
 const mongoose = require('mongoose');
@@ -18,30 +17,60 @@ module.exports = app => {
 });
 
   app.post('/soccerapp/game/webhooks', (req, res) => {
-    const p = new Path('soccerapp/game/:gameId/:choice');
+        // console.log(req.body)
+        const p = new Path('/soccerapp/game/:gameId/:choice');
+        // console.log(p)
+        _.chain(req.body)
+          .map(({ email, url }) => {
+            const match = p.test(new URL(url).pathname);
+            console.log(match);
+            if (match) {
+              return {
+                email,
+                gameId: match.gameId,
+                choice: match.choice
+              };
+            }
+          })
+          .compact()
+          .uniqBy('email', 'gameId')
+          .each(({
+            gameId,
+            email,
+            choice
+          }) => {
+            Game.updateOne({
+              _id: gameId,
+              recipients: {
+                $elemMatch: {
+                  email: email,
+                  responded: false
+                }
+              }
+            }, {
+              $inc: {
+                [choice]: 1
+              },
+              $set: {
+                'recipients.$.responded': true
+              },
+              lastResponded: new Date()
+            }).exec();
+          })
+          .value();
 
-    _.chain(req.body)
-      .map(({ email, url }) => {
-        const match = p.test(new URL(url).pathname);
-        if (match) {
-          return { email, gameId: match.gameId, choice: match.choice };
-        }
-        })
-      .compact()
-      .uniqBy( 'email', 'gameId' )
-      .value();
-      res.send({})
+        res.send({});
     });
 
 
-  app.post(
-    '/soccerapp/game', requireLogin, async (req, res) => {
-      const { title, subject, body, date, recipients } = req.body;
+  app.post('/soccerapp/game', requireLogin, async (req, res) => {
+      const { title, subject, body, time, date, recipients } = req.body;
 
       const game = new Game({
         title,
         subject,
         body,
+        time,
         date,
         recipients: recipients.split(',').map(email => ({ email: email.trim() })),
         _user: req.user.id,
@@ -51,10 +80,10 @@ module.exports = app => {
       // Here we send a new email:
       const mailer = new Mailer(game, emailTemplate(game));
       try {
-      await mailer.send();
-      await game.save();
-    } catch (err) {
-      res.status(422).send(err);
-    }
+        await mailer.send();
+        await game.save();
+      } catch (err) {
+        res.status(422).send(err);
+      }
     });
 };
